5aba9427e2e478bb51fb0e0f06876af549e1c95b
Chris Taylor <christaylor801@gmail.com>
2020-02-20 21:59:59 -0700

    CI Tests

--- Tests/ConnectWiseManageAPI.Tests.ps1
+++ Tests/ConnectWiseManageAPI.Tests.ps1
@@ -14,14 +14,13 @@ Describe "Connect-CWM  PS$PSVersion Integrations tests" {
     Context 'Strict mode' { 
 
         Set-StrictMode -Version latest
-        # API key
-        It 'should connect with api key' {
+        It 'Authentication: API Member Only' {
             try {
                 $CWMConnectionInfo = @{
                     Server = $CWMServer
                     Company = $env:CWMCompany
-                    pubkey = $env:CWMPub
-                    privatekey = $env:CWMPriv
+                    pubkey = $env:CWMAPIMemberPub
+                    privatekey = $env:CWMAPIMemberPriv
                     clientid = $env:CWMClientID
                 }
 
@@ -33,56 +32,52 @@ Describe "Connect-CWM  PS$PSVersion Integrations tests" {
             }
             $Connected | Should -Be $true
         }
-        It 'should fail with invalid api key' {
+        It 'Authentication: Member Impersonation' {
             try {
                 $CWMConnectionInfo = @{
                     Server = $CWMServer
+                    IntegratorUser = $env:CWMAPIMemberPub
+                    IntegratorPass = $env:CWMAPIMemberPriv
                     Company = $env:CWMCompany
-                    pubkey = '123'
-                    privatekey = $env:CWMPriv
+                    MemberID = $env:CWMUserName
                     clientid = $env:CWMClientID
                 }
-
+                
                 Connect-CWM @CWMConnectionInfo -Force -ErrorAction Stop
                 $Connected = $true                   
             }
             catch {
                 $Connected = $false 
             }
-            $Connected | Should -Be $false
+            $Connected | Should -Be $true
         }
-        # Client ID
-        It 'should fail with invalid Client ID' {
+        It 'Authentication: My Account' {
             try {
                 $CWMConnectionInfo = @{
                     Server = $CWMServer
                     Company = $env:CWMCompany
                     pubkey = $env:CWMPub
                     privatekey = $env:CWMPriv
-                    clientid = '123'
+                    clientid = $env:CWMClientID
                 }
-
                 Connect-CWM @CWMConnectionInfo -Force -ErrorAction Stop
                 $Connected = $true                   
             }
             catch {
                 $Connected = $false 
             }
-            $Connected | Should -Be $false
+            $Connected | Should -Be $true
         }
-        # Cookie
-        It 'should connect with username and password' {
+        It 'Authentication: Cookies!' {
             try {
                 $Password = ConvertTo-SecureString -AsPlainText -Force $env:CWMUserPassword
                 $Credentials = New-Object PSCredential $env:CWMUserName, $Password
-
                 $CWMConnectionInfo = @{
                     Server = $CWMServer
                     Company = $env:CWMCompany
                     Credentials = $Credentials
-                    clientid = $env:CWMClientID
+                    clientid = $ClientID
                 }
-
                 Connect-CWM @CWMConnectionInfo -Force -ErrorAction Stop
                 $Connected = $true                   
             }
