36b5876a5c36f921c15f0a789987dc5987e4dbf7
Chris Taylor <christaylor801@gmail.com>
2020-02-20 21:03:01 -0700

    CI test

--- Tests/ConnectWiseManageAPI.Tests.ps1
+++ Tests/ConnectWiseManageAPI.Tests.ps1
@@ -1,3 +1,4 @@
+$CWMServer = 'https://staging.connectwisedev.com/'
 $Verbose = @{}
 if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
 {
@@ -8,15 +9,29 @@ $PSVersion = $PSVersionTable.PSVersion.Major
 Import-Module $PSScriptRoot\..\ConnectWiseManageAPI\ConnectWiseManageAPI.psm1 -Force
 
 #Integration test example
-Describe "Get-WebrootHealthCheckVersion  PS$PSVersion Integrations tests" {
+Describe "Connect-CWM  PS$PSVersion Integrations tests" {
 
     Context 'Strict mode' { 
 
         Set-StrictMode -Version latest
+        # Connection test
+        It 'should connect with api key' {
+            try {
+                $CWMConnectionInfo = @{
+                    Server = $CWMServer
+                    Company = $env:CWMCompany
+                    pubkey = $env:CWMPub
+                    privatekey = $env:CWMPriv
+                    clientid = $env:CWMClientID
+                }
 
-        It 'should get valid data' {
-            $Output = $True 
-            $Output -ne $False
+                Connect-CWM @CWMConnectionInfo -ErrorAction Stop
+                $Connected = $true                   
+            }
+            catch {
+                $Connected = $false 
+            }
+            $Connected | Should -Be $true
         }
     }
 }
