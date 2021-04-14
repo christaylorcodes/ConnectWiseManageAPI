07a0c25d67021530d270cafe6457029baba2df0d
Kevin Cowgur <53311952+Nerdshade@users.noreply.github.com>
2021-04-14 13:59:04 -0500

    Added new functions Get-CWMTicketTask and New-CWMTicketTask to add and view tasks on existing tickets.

---
+++ ConnectWiseManageAPI/Public/Service/TicketTasks/Get-CWMTicketTask.ps1
@@ -0,0 +1,27 @@
+﻿function Get-CWMTicketTask {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [int]$TicketID,
+        [string]$Condition,
+        [ValidatePattern('\S* (desc|asc)')]
+        [string]$orderBy,
+        [string]$childConditions,
+        [string]$customFieldConditions,
+        [int]$page,
+        [int]$pageSize,
+        [string[]]$fields,
+        [switch]$all,
+        [string]$TaskID
+    )
+
+    If ($TaskID){
+        $URI = "https://$($script:CWMServerConnection.Server)/v4_6_release/apis/3.0/service/tickets/$($TicketID)/tasks/$($TaskID)"
+    }
+    Else{
+        $URI = "https://$($script:CWMServerConnection.Server)/v4_6_release/apis/3.0/service/tickets/$($TicketID)/tasks"
+    }
+
+    return Invoke-CWMGetMaster -Arguments $PsBoundParameters -URI $URI
+
+}
---
+++ ConnectWiseManageAPI/Public/Service/TicketTasks/New-CWMTicketTask.ps1
@@ -0,0 +1,16 @@
+﻿function New-CWMTicketTask {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess','', Justification = 'Used by sub-function')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
+    param(
+        [int]$id,
+        [int]$TicketID,
+        [string]$Notes,
+        [boolean]$ClosedFlag,
+        [int]$Priority,
+        [string]$Resolution,
+        $_info
+    )
+
+    $URI = "https://$($script:CWMServerConnection.Server)/v4_6_release/apis/3.0/service/tickets/$ticketId/tasks"
+    return Invoke-CWMNewMaster -Arguments $PsBoundParameters -URI $URI
+}
