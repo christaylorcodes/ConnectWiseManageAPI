19c6d50663533c85c18c37f8f5ec90ffe80e46b2
Chris Taylor <36066223+christaylorcodes@users.noreply.github.com>
2021-04-29 23:06:46 -0600

    Removed Comments

--- ConnectWiseManageAPI/Public/Company/Configurations/Get-CWMCompanyConfigurationTypes.ps1
+++ ConnectWiseManageAPI/Public/Company/Configurations/Get-CWMCompanyConfigurationTypes.ps1
@@ -1,21 +1,4 @@
 ﻿function Get-CWMCompanyConfigurationTypes {
-<#    [CmdletBinding()]
-    param(
-        [string]$Condition,
-        [ValidatePattern('\S* (desc|asc)')]
-        [string]$orderBy,
-        [string]$childConditions,
-        [string]$customFieldConditions,
-        [int]$page,
-        [int]$pageSize,
-        [string[]]$fields,
-        [switch]$all
-    )
-
-    $URI = "https://$($script:CWMServerConnection.Server)/v4_6_release/apis/3.0/company/configurations/types"
-
-    return Invoke-CWMGetMaster -Arguments $PsBoundParameters -URI $URI
-#>
     [CmdletBinding()]
     param(
         [int]$id,
