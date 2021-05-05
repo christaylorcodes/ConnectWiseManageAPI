6729c7a2507bea8f062c8f0e3dfa2a96af7f313d
Chris Taylor <36066223+christaylorcodes@users.noreply.github.com>
2021-05-04 21:29:43 -0600

    New-CWMDocument
    Cleanup form creation.
    Add support for all parameters.

--- ConnectWiseManageAPI/Public/System/Documents/New-CWMDocument.ps1
+++ ConnectWiseManageAPI/Public/System/Documents/New-CWMDocument.ps1
@@ -1,4 +1,4 @@
-﻿function New-CWMDocument {
+function New-CWMDocument {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Used by sub-function')]
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
     param(
@@ -24,14 +24,11 @@
         [string]$title,
         [Parameter(Mandatory=$true, ParameterSetName = 'URL')]
         [string]$URL,
-        [Parameter(
-            Mandatory=$true,
-            ParameterSetName = 'File'
-        )]
+        [Parameter(Mandatory=$true, ParameterSetName = 'File')]
         [ValidateScript({
-            if(-Not ($_ | Test-Path)){ throw "File or folder does not exist" }
+            if(-Not ($_ | Test-Path)){ throw 'File or folder does not exist' }
             if(-Not ($_ | Test-Path -PathType Leaf)){
-                throw "The Path argument must be a file. Folder paths are not allowed."
+                throw 'The Path argument must be a file. Folder paths are not allowed.'
             }
             $true
         })]
@@ -39,73 +36,72 @@
         [Parameter(ParameterSetName = 'File')]
         [string]$FileName,
         [boolean]$Private = $true,
-        [boolean]$ReadOnly = $false
+        [boolean]$ReadOnly = $false,
+        [boolean]$isAvatar = $false
     )
-    $boundary = [System.Guid]::NewGuid().ToString();
-    $LF = "`r`n";
-    switch ($PSCmdlet.ParameterSetName){
-        "File" {
-            Write-Debug "This is the File ParameterSet"
-            If(!$FileName){
-                Write-Debug "Making default filename from `$Filepath"
-                $Filename = Split-Path $FilePath -Leaf
+
+    Process{
+        # https://www.techcolumnist.com/2019/01/09/powershell-connectwise-documents-api-uploading-a-document-or-attachment-to-a-ticket
+        $boundary = [System.Guid]::NewGuid().ToString()
+
+        $Body = (
+            "--$boundary",
+            'Content-Disposition: form-data; name="recordType"',
+            '',
+            $recordType,
+            "--$boundary",
+            'Content-Disposition: form-data; name="recordId"',
+            '',
+            $recordId,
+            "--$boundary",
+            'Content-Disposition: form-data; name="Title"',
+            '',
+            $title,
+            "--$boundary",
+            'Content-Disposition: form-data; name="PrivateFlag',
+            '',
+            $Private,
+            "--$boundary",
+            'Content-Disposition: form-data; name="ReadOnlyFlag"',
+            '',
+            $ReadOnly,
+            "--$boundary",
+            'Content-Disposition: form-data; name="isAvatar"',
+            '',
+            $isAvatar,
+            "--$boundary"
+        )
+
+        switch ($PSCmdlet.ParameterSetName){
+            'File' {
+                if(!$FileName){ $FileName = Split-Path $FilePath -Leaf }
+
+                $fileBytes = [System.IO.File]::ReadAllBytes($FilePath)
+                $fileEnc = [System.Text.Encoding]::GetEncoding(28591).GetString($fileBytes)
+
+                $Body += (
+                    "Content-Disposition: form-data; name=`"file`"; filename=`"$FileName`"",
+                    'Content-Type: application/octet-stream',
+                    '',
+                    $fileEnc,
+                    "--$boundary--"
+                )
             }
-            # https://www.techcolumnist.com/2019/01/09/powershell-connectwise-documents-api-uploading-a-document-or-attachment-to-a-ticket/
-            $fileBytes = [System.IO.File]::ReadAllBytes($FilePath);
-            $fileEnc = [System.Text.Encoding]::GetEncoding(28591).GetString($fileBytes);
-            $Body = (
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"recordType`"$LF",
-                "$recordType",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"recordId`"$LF",
-                "$recordId",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"Title`"$LF",
-                "$title",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"PrivateFlag`"$LF",
-                "$Private",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"ReadOnlyFlag`"$LF",
-                "$ReadOnly",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"file`"; filename=`"$FileName`"",
-                "Content-Type: application/octet-stream$LF",
-                $fileEnc,
-                "--$boundary--$LF"
-            ) -join $LF
+            'URL' {
+                $Body += (
+                    'Content-Disposition: form-data; name="url"',
+                    '',
+                    $URL,
+                    "--$boundary--"
+                )
+            }
         }
-        "URL" {
-            Write-Debug "This is the URL ParameterSet"
-            $Body = (
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"recordType`"$LF",
-                "$recordType",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"recordId`"$LF",
-                "$recordId",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"Title`"$LF",
-                "$title",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"PrivateFlag`"$LF",
-                "$Private",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"ReadOnlyFlag`"$LF",
-                "$ReadOnly",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"url`"$LF",
-                "$URL",
-                "--$boundary",
-                "Content-Disposition: form-data; name=`"urlFlag`"$LF",
-                "$true",
-                "--$boundary--$LF"
-            ) -join $LF
-        }
+
+        $PsBoundParameters.Body = $Body | Out-String
+        $PsBoundParameters.ContentType = "multipart/form-data; boundary=`"$boundary`""
+
+        $Endpoint = '/system/documents'
+
+        Invoke-CWMNewMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
     }
-    $PsBoundParameters.Body = $Body
-    $PsBoundParameters.ContentType = "multipart/form-data; boundary=`"$boundary`""
-    $Endpoint = '/system/documents'
-    Invoke-CWMNewMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
 }
