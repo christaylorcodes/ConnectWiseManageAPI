ebb122a4a37849c9e0830478c5022549a7a84578
AdeleTheFarmer <github@jimh.co>
2023-10-04 13:53:08 +0100

    Added AgreementType Endpoints
    Added New-CWMAgreementTypes, Update-CWMAGreementType and New-CWMAGreementType Endpoints.
    Modified New-CWMAgreement to include ShipToCompany and ShipToSite params.

---
+++ ConnectWiseManageAPI/Public/Finance/AgreementTypes/Get-CWMAgreementTypes.ps1
@@ -0,0 +1,20 @@
+﻿function Get-CWMAgreementTypes {
+    [CmdletBinding()]
+    param(
+        [int]$id,
+        [switch]$count,
+        [string]$condition,
+        [ValidatePattern('\S* (desc|asc)')]
+        [string]$orderBy,
+        [string]$childConditions,
+        [string]$customFieldConditions,
+        [int]$page,
+        [int]$pageSize,
+        [string[]]$fields,
+        [switch]$all
+    )
+
+    $Endpoint = '/finance/agreements/types'
+    Invoke-CWMGetMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
+
---
+++ ConnectWiseManageAPI/Public/Finance/AgreementTypes/New-CWMAgreementTypes.ps1
@@ -0,0 +1,84 @@
+function New-CWMAgreementTypes {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    param(
+        [int]$id,
+        [Parameter(Mandatory = $true)]
+        [ValidateLength(1, 100)]
+        [string]$name,
+        [ValidateSet('Prefix', 'Suffix')]
+        [string]$prefixSuffixOption,
+        [boolean]$defaultFlag,
+        [boolean]$inactiveFlag,
+        [boolean]$prePaymentFlag,
+        [string]$invoicePreSuffix,
+        [hashtable]$location,
+        [hashtable]$department,
+        [boolean]$restrictLocationFlag,
+        [boolean]$restrictDepartmentFlag,
+        [hashtable]$sla,
+        [ValidateSet('Amount', 'Hours', 'Incidents')]
+        [string]$applicationUnits,
+        [double]$applicationLimit,
+        [ValidateSet('Contract2Weeks', 'Contract4Weeks', 'ContractYear', 'CalendarMonth', 'CalendarQuarter', 'CalendarWeek', 'ContractQuarter', 'CalendarYear')]
+        [string]$applicationCycle,
+        [boolean]$applicationUnlimitedFlag,
+        [boolean]$oneTimeFlag,
+        [boolean]$coverAgreementTimeFlag,
+        [boolean]$coverAgreementProductFlag,
+        [boolean]$coverAgreementExpenseFlag,
+        [boolean]$coverSalesTaxFlag,
+        [boolean]$carryOverUnusedFlag,
+        [boolean]$allowOverrunsFlag,
+        [int]$expiredDays,
+        [int]$limit,
+        [boolean]$expireWhenZero,
+        [boolean]$chargeToFirmFlag,
+        [ValidateSet('Actual', 'Hourly')]
+        [string]$employeeCompRate,
+        [ValidateSet('Billing', 'Amount', 'Percent')]
+        [string]$employeeCompNotExceed,
+        [double]$compHourlyRate,
+        [double]$compLimitAmount,
+        [hashtable]$billingCycle,
+        [boolean]$billOneTimeFlag,
+        [hashtable]$billingTerms,
+        [ValidateSet('ContractYear', 'CalendarYear')]
+        [string]$invoicingCycle,
+        [double]$billAmount,
+        [boolean]$taxableFlag,
+        [boolean]$restrictDownPaymentFlag,
+        [string]$invoiceDescription,
+        [boolean]$topCommentFlag,
+        [boolean]$bottomCommentFlag,
+        [hashtable]$workRole,
+        [hashtable]$workType,
+        [hashtable]$projectType,
+        [hashtable]$invoiceTemplate,
+        [ValidateSet('Billable', 'DoNotBill', 'NoCharge', 'NoDefault')]
+        [string]$billTime,
+        [ValidateSet('Billable', 'DoNotBill', 'NoCharge', 'NoDefault')]
+        [string]$billExpenses,
+        [ValidateSet('Billable', 'DoNotBill', 'NoCharge', 'NoDefault')]
+        [string]$billProducts,
+        [boolean]$billableTimeInvoiceFlag,
+        [boolean]$billableExpenseInvoiceFlag,
+        [boolean]$billableProductInvoiceFlag,
+        [boolean]$copyWorkRolesFlag,
+        [boolean]$copyWorkTypesFlag,
+        [array]$exclusionWorkRoleIds,
+        [boolean]$addAllWorkRoleExclusions,
+        [boolean]$removeAllWorkRoleExclusions,
+        [array]$exclusionWorkTypeIds,
+        [boolean]$addAllWorkTypeExclusions,
+        [boolean]$removeAllWorkTypeExclusions,
+        [string]$integrationXRef,
+        [boolean]$prorateFlag,
+        [hashtable]$emailTemplate,
+        [boolean]$autoInvoiceFlag,
+        [boolean]$invoiceProratedAdditionsFlag,
+        [hashtable]$_info
+    )
+
+    $Endpoint = '/finance/agreement/types'
+    Invoke-CWMNewMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
