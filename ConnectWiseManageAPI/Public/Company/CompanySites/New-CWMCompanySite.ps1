2a67eae66eb91b9a9ebbb7360f589711e19f35fe
dawsobr2 <108246784+dawsobr2@users.noreply.github.com>
2024-04-20 17:31:09 -0700

    Added New/Update/Remove verbs for CWMCompanySite

---
+++ ConnectWiseManageAPI/Public/Company/CompanySites/New-CWMCompanySite.ps1
@@ -0,0 +1,37 @@
+﻿function New-CWMCompanySite {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Used by sub-function')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('companyId')]
+        [int]$parentId,
+        [int]$id,
+        [Parameter(Mandatory = $true)]
+        [string]$name,
+        [string]$addressLine1,
+        [string]$addressLine2,
+        [string]$city,
+        [hashtable]$stateReference,
+        [string]$zip,
+        [hashtable]$country,
+        [string]$phoneNumber,
+        [string]$phoneNumberExt,
+        [string]$faxNumber,
+        [hashtable]$taxCode,
+        [hashtable]$entityType,
+        [double]$expenseReimbursement,
+        [bool]$primaryAddressFlag,
+        [bool]$defaultShippingFlag,
+        [bool]$defaultBillingFlag,
+        [bool]$defaultMailingFlag,
+        [bool]$inactiveFlag,
+        [bool]$billSeperateFlag,
+        [string]$mobileGuid,
+        [hashtable]$calendar,
+        [hashtable]$timeZone,
+        [hashtable]$customFields
+    )
+
+    $Endpoint = "/company/companies/$($parentId)/sites"
+    Invoke-CWMNewMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
---
+++ ConnectWiseManageAPI/Public/Company/CompanySites/Update-CWMCompanySite.ps1
@@ -0,0 +1,21 @@
+﻿function Update-CWMCompanySite {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Used by sub-function')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('companyId')]
+        [int]$parentId,
+        [Parameter(Mandatory = $true)]
+        [int]$id,
+        [Parameter(Mandatory = $true)]
+        [validateset('add', 'replace', 'remove')]
+        [string]$Operation,
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+        [Parameter(Mandatory = $true)]
+        $Value
+    )
+
+    $Endpoint = "/company/companies/$($parentId)/sites/$($id)"
+    Invoke-CWMUpdateMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
