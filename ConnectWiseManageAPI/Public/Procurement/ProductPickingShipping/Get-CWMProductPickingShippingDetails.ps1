ef05b91f14e029128147ef4b8e0c08922022e71f
Damian West <damian@damian.id.au>
2024-10-12 19:23:44 +1100

    Added *-CWMProductPickingShippingDetails

---
+++ ConnectWiseManageAPI/Public/Procurement/ProductPickingShipping/Get-CWMProductPickingShippingDetails.ps1
@@ -0,0 +1,21 @@
+function Get-CWMProductPickingShippingDetails {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [int]$parentId,
+        [int]$id,
+        [switch]$count,
+        [string]$condition,
+        [ValidatePattern('\S* (desc|asc)')]
+        [string]$orderBy,
+        [string]$childConditions,
+        [string]$customFieldConditions,
+        [int]$page,
+        [int]$pageSize,
+        [string[]]$fields,
+        [switch]$all
+    )
+
+    $Endpoint = "/procurement/products/$($parentId)/pickingShippingDetails"
+    Invoke-CWMGetMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
