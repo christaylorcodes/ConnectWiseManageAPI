7243d1ab16a7b1a141a1f4b81bd6b7b9e7fe9d63
Alex Kendrick <alex@alex-kendrick.com>
2024-12-25 12:56:36 -0500

    Add Update-CWMOpportunity

---
+++ ConnectWiseManageAPI/Public/Sales/Opportunities/Update-CWMOpportunity.ps1
@@ -0,0 +1,17 @@
+﻿function Update-CWMOpportunity {
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('opportunityID')]
+        [int]$id,
+        [Parameter(Mandatory = $true)]
+        [validateset('add', 'replace', 'remove')]
+        [string]$Operation,
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+        [Parameter(Mandatory = $true)]
+        $Value
+    )
+
+    $Endpoint = "/sales/opportunities/$($id)"
+    Invoke-CWMUpdateMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
