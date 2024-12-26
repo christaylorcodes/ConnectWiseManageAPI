65eec1ddb9b53e2b6ecf4d43fad44a99615a49f7
Alex Kendrick <alex@alex-kendrick.com>
2024-12-26 17:49:26 -0500

    Add New-CWMOpportunityForecast and New-CWMOpportunityForecastItem

---
+++ ConnectWiseManageAPI/Public/Sales/Opportunities/Forecast/New-CWMOpportunityForecast.ps1
@@ -0,0 +1,32 @@
+function New-CWMOpportunityForecast {
+    param (
+        [Parameter(Mandatory=$true)]
+        [int]$parentId,
+        [int]$id = 0,
+        [array]$forecastItems,
+        [hashtable]$productRevenue,
+        [hashtable]$serviceRevenue,
+        [hashtable]$agreementRevenue,
+        [hashtable]$timeRevenue,
+        [hashtable]$expenseRevenue,
+        [hashtable]$forecastRevenueTotals,
+        [hashtable]$inclusiveRevenueTotals,
+        [decimal]$recurringTotal = 0,
+        [hashtable]$wonRevenue,
+        [hashtable]$lostRevenue,
+        [hashtable]$openRevenue,
+        [hashtable]$otherRevenue1,
+        [hashtable]$otherRevenue2,
+        [decimal]$salesTaxRevenue = 0,
+        [decimal]$forecastTotalWithTaxes = 0,
+        [decimal]$expectedProbability = 0,
+        [hashtable]$taxCode,
+        [hashtable]$billingTerms,
+        [hashtable]$currency,
+        [hashtable]$_info
+    )
+
+    $endpoint = "/sales/opportunities/$parentId/forecast"
+
+    Invoke-CWMNewMaster -Arguments $PSBoundParameters -Endpoint $endpoint
+}
